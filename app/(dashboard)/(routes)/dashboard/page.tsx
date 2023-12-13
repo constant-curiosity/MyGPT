cf0fb29e24ea23790814b8872c30d81e80c4ca41
name <dev.constant@tutanota.com>
2023-12-13 16:59:32 +0000

    added: Navbar component / (dashboard)/layout

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
   return (
     <div className="text-6xl flex flex-col items-center justify-center h-screen">
       <p>Dashboard Page (Protected)</p>
+      <div className="flex py-4 px-4">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
-    <div className="text-6xl flex flex-col items-center justify-center h-screen">
-      Landing Page
-    </div>
+    <>
+      <div className="text-6xl flex flex-col items-center justify-center h-screen">
+        Landing Page
+        <div className="flex">
+          <div className="mr-2">
+            <Link href={"/sign-in"}>
+              <Button className="bg-blue-500 text-white px-4 py-2 rounded">
+                Login
+              </Button>
+            </Link>
+          </div>
+
+          <div>
+            <Link href={"/sign-up"}>
+              <Button className="bg-blue-500 text-white px-4 py-2 rounded">
+                Register
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
