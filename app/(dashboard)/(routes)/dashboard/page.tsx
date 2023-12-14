21ce4922a917073c996acf5163d64c28d4367ee7
name <dev.constant@tutanota.com>
2023-12-14 15:16:39 +0000

    added: navbar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
   return (
     <div className="text-6xl flex flex-col items-center justify-center h-screen">
       <p>Dashboard Page (Protected)</p>
-      <div className="flex py-4 px-4">
-        <UserButton afterSignOutUrl="/" />
-      </div>
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
 
@@ -7,6 +8,12 @@ export default function Navbar() {
       <Button variant="ghost" size="icon" className="md:hidden">
         <Menu />
       </Button>
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
   );
 }
+
+//Remove the "p-4" (<div className="flex items-center p-4">) because it was adding extra screen height, might need
+//to be added back in the future.
